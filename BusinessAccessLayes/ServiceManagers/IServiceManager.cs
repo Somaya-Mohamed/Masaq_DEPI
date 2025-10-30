bb87e9e72a47ddf0425d1d9221cf1966345c680d
Tasneem224 <tasneemtaha224@gmail.com>
2025-10-30 16:24:35 +0300

    resolve

--- BusinessAccessLayes/ServiceManagers/IServiceManager.cs
+++ BusinessAccessLayes/ServiceManagers/IServiceManager.cs
@@ -10,18 +10,9 @@ namespace BusinessAccessLayes.ServiceManagers
 {
     public interface IServiceManager
     {
-<<<<<<< HEAD
-        public ILessonService LessontService { get; }
-
-        public IAuthenticationService AuthenticationService { get; }
-=======
         public ILessonService LessonService { get; }
-
         public IAnncouncementService AnnouncementService { get; }
         public ICommentService CommentService { get; }
-        // --- ده الكود المدمج ---
-        //public IAuthenticationService AuthenticationService { get; }
-
->>>>>>> TasneemWork
+        public BusinessAccessLayes.Services.Interfaces.IAuthenticationService AuthenticationService { get; }
     }
 }
